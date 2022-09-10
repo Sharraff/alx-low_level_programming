6d18aafb60001fef49614636a98855c85d2f4d7c
root <root@DESKTOP-67TLRHA.localdomain>
2022-09-10 07:31:09 -0700

    program that prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	int d = 48;
+	int a = 48;
+
+	while (d < 58)
+	{
+		a = 48;
+		while (a < 58)
+		{
+			if (d != a && d < a)
+			{
+				putchar(d);
+				putchar(a);
+
+				if (!(d == 56 && a == 57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			a++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
