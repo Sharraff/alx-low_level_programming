707a583c2f89ca577c6006575fee25167816e4c0
root <root@DESKTOP-67TLRHA.localdomain>
2022-09-13 12:14:19 -0700

    Write a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - prints the word _putchar
+ *
+ * Return: 0 on successfull execution
+ */
+int main()
+{
+	char word[8] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	_putchar(8);
+	return (0);
+}
