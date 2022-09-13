bb73e83e0b4199afd58f5c2dcaa3ec41dda7dc0c
root <root@DESKTOP-67TLRHA.localdomain>
2022-09-13 12:29:47 -0700

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,15 @@
  */
 int main(void)
 {
-	char word[10] = "_putchar";
-	int i = 0;
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	for (i = 0; i < 10; i++)
-	{
-		_putchar(word[i]);
-		i++;
-	}
-	_putchar(10);
 	return (0);
 }
