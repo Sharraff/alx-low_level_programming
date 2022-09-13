faeb1c091de002a39996e6eb6e45a5ae6f0616fd
root <root@DESKTOP-67TLRHA.localdomain>
2022-09-13 15:17:44 -0700

    this function checks for lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - this function checks for lowercase characters
+ *
+ * Return: Always 0
+ *
+ *@c: character to compare
+ */
+int _islower(int c)
+{
+	char i;
+	int lower;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		if (i == c)
+			lower = 1;
+	}
+	return (lower);
+}
