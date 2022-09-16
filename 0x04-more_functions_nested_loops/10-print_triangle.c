32887546d09ef7090e95a44110ebb3ed90b52649
root <root@DESKTOP-67TLRHA.localdomain>
2022-09-16 06:46:03 -0700

    this function prints triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangles - prints triangle using "#" character of a given size
+ * @size: size of triangle
+ * Return: Always 0
+ */
+void print_triangles(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= size; i++)
+		{
+			for (j = size - 1 ; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
