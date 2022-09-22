839ad934c173681ffef7575e62360412d8480239
root <root@DESKTOP-67TLRHA.localdomain>
2022-09-22 03:44:33 -0700

    hot fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,11 +8,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int = 0;
-
-	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
+	while (*s1 && *s1 == *s2)
 	{
-		i++;
+		s1++;
+		s2++;
 	}
-	return (s1[i] - s2[i]);
+	return ((int)(unsigned char)(*s1) - (int)(unsigned char)(*s2));
 }
