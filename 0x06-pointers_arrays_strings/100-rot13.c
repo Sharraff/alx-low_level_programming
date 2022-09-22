dafa4d1929e71debc83dfa0fd133ae4f27b08b63
root <root@DESKTOP-67TLRHA.localdomain>
2022-09-22 06:18:36 -0700

    this function implement the rot13 algorithm

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.c"
+/**
+ * rot13 - converts string to leet
+ *
+ * @c: character string pointer
+ * Return: char pointer
+ */
+char *rot13(char *c)
+{
+	int i;
+	int j;
+	char rot_13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (alphabet[j] == c[i])
+			{
+				c[i] == rot_13[j];
+				break;
+			}
+		}
+	}
+	return (c);
+}
