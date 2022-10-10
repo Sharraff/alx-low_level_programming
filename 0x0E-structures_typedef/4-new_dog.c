07d6e414b98d2e9f8e34dcb505d55924a0e44b25
root <root@DESKTOP-67TLRHA.localdomain>
2022-10-10 09:35:00 -0700

    fixing stuff

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,58 +2,49 @@
 #include <stdlib.h>
 
 /**
- * new_dog - new element of type struct dog
- *
- * @name: name of new dog
- * @age: age of new dog
- * @owner: owner of new dog
- * Return: new struct dog
+ * new_dog - creates a new dog structure
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Return: returns a pointer to newly created dog structure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *dog_name;
-	char *dog_owner;
+	char *cpyname, *cpyowner;
+	int len_name = 0, len_owner = 0, i;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+
+	while (name[len_name])
+		len_name++;
+	while (owner[len_owner])
+		len_owner++;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-		return (new_dog);
-	dog_name = malloc(sizeof(name));
-	if (dog_name == NULL)
-	{
-		free(new_dog);
 		return (NULL);
-	}
-	dog_owner = malloc(sizeof(owner));
-	if (dog_owner == NULL)
-	{
-		free(new_dog);
+
+	cpyname = malloc(len_name + 1);
+	if (cpyname == NULL)
 		return (NULL);
-	}
-	_strcpy(dog_name, name);
-	_strcpy(dog_owner, owner);
-	new_dog->name = dog_name;
-	new_dog->owner = dog_owner;
+	for (i = 0; name[i]; i++)
+		cpyname[i] = name[i];
+	cpyname[i] = '\0';
+
+
+	cpyowner = malloc(len_owner + 1);
+	if (cpyowner == NULL)
+		return (NULL);
+	for (i = 0; owner[i]; i++)
+		cpyowner[i] = owner[i];
+	cpyowner[i] = '\0';
+
+
+	new_dog->name = cpyname;
 	new_dog->age = age;
+	new_dog->owner = cpyowner;
 	return (new_dog);
-}
-/**
- *_strcpy - copies string to given memory location
- * @dest: where the string needs to be copied
- * @src: where the string is
- *
- * Return: char
- */
-char *_strcpy(char *dest, char *src)
-{
-	int len = 0;
-
-	while (*(src + len) != '\0')
-	{
-		*(dest + len) = *(src + len);
-		len++;
-	}
-	*(dest + len) = *(src + len);
-
-	return (dest);
 }
