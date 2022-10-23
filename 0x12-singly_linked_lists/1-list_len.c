45e3aadcf814d39e3458daae1973632544e13e4e
root <root@DESKTOP-67TLRHA.localdomain>
2022-10-23 12:10:16 -0700

    hot fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,15 +10,12 @@
 size_t list_len(const list_t *h)
 {
 	char *str;
-	unsigned int len;
 	int i = 0;
 	unsigned int count = 0;
 
 
 	for (i = 0; h != NULL; i++)
 	{
-		if (str == NULL)
-			printf("(nill)");
 		h = h->next;
 		count++;
 	}
