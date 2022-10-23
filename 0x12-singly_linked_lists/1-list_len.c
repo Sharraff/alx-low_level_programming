cd5babc7de0c63c383f54eba536b861dbe3e399f
root <root@DESKTOP-67TLRHA.localdomain>
2022-10-23 11:52:46 -0700

    this function calculates the number of elements in a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *list_len - this function counts the number of elements in a list.
+ *
+ *@h: pointer to next element of node
+ *Return: return count
+ */
+size_t list_len(const list_t *h)
+{
+	char *str;
+	unsigned int len;
+	int i = 0;
+	unsigned int count = 0;
+
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (str == NULL)
+			printf("(nill)");
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
