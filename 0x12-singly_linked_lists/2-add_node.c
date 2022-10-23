8dbd8bd75976beb92013593819db0ea9b9ae3083
root <root@DESKTOP-67TLRHA.localdomain>
2022-10-23 14:15:04 -0700

    changes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,9 +4,11 @@
 #include <string.h>
 
 /**
+ * add_node - A function that adds a node to the head of linked list
  *
- *
- *
+ * @head: pointer to a list_t pointer that points to the head struct
+ * @str: pointer to string that adds as node
+ * Return: pointer to new head of list, NULL on failure
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -21,14 +23,12 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
-	
+
 
 	node->len = lenght;
 	node->str = strdup(str);
 	node->next = *head;
 	*head = node;
-
-
 
 	return (node);
 }
