eca2e33df1f94a4edc4228a0ba911200e2716eaa
root <root@DESKTOP-67TLRHA.localdomain>
2023-01-06 07:30:06 -0800

    minor fix

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,41 +1,39 @@
 #include "hash_tables.h"
-#include <stdio.h>
-#include <string.h>
-
-int not_last_node(const hash_table_t *ht, hash_node_t *node);
+#include <stdlib.h>
 
 /**
- * hash_table_print - prints the contents of a hash table
- * @ht: hash table to print
+ * hash_table_delete - A function that frees a hash table.
+ * @ht: The pointer to hash table to delete.
+ * Return: Nothing.
  */
-void hash_table_print(const hash_table_t *ht)
+void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i = 0, last_pos = 0;
-	hash_node_t *node = NULL;
+	unsigned long int i = 0;
+	hash_node_t *next_node = NULL, *node = NULL;
 
-	if (ht == NULL)
+	if (!ht)
 		return;
-	putchar('{');
-	if (ht)
+
+	if (ht->array)
 	{
-		for (; i < ht->size - 1; i++)
+		for (; i < ht->size; i++)
 		{
-			if (ht->array[i] != NULL)
-				last_pos = i;
-		}
-
-		for (i = 0; i <= last_pos; i++)
-		{
-			node = ht->array[i];
-			while (node)
+			if (ht->array[i])
 			{
-				printf("'%s': '%s'", node->key, node->value);
-				node = node->next;
-				if (i < last_pos - 1)
-					printf(", ");
+				node = ht->array[i];
+				while (node)
+				{
+					next_node = node->next;
+					if (node->value)
+						free(node->value);
+					if (node->key)
+						free(node->key);
+					free(node);
+					node = next_node;
+				}
 			}
-
 		}
+		free(ht->array);
 	}
-	printf("}\n");
+	free(ht);
 }
